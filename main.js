59182f0de9563457ccef32e19f039b28f26846ff
Anvar Nazar <anvarnasar@ymail.com>
2021-07-25 14:35:24 +0530

    include and global exclude headers from requests

---
+++ main.js
@@ -0,0 +1,66 @@
+async function setHeaders(context) {
+    const isExcluded = await context.store.hasItem(`excluded.${context.request.getId()}`);
+    console.log(isExcluded);
+    if (isExcluded) {
+        console.log("[global-headers]: Skipping... request is excluded");
+        return false;
+    }
+
+    const globalHeaders = await context.request.getEnvironmentVariable("GLOBAL_HEADERS");
+    if (!globalHeaders) {
+        console.log("[global-headers]: Skipping... no [GLOBAL_HEADERS] present in environment");
+        return;
+    }
+
+    for (const headerName of Object.keys(globalHeaders)) {
+        const headerValue = globalHeaders[headerName];
+
+        if (await context.request.hasHeader(headerName)) {
+            console.log("[global-headers]: Skipping... header already set");
+            continue;
+        }
+
+        await context.request.setHeader(headerName, headerValue);
+    }
+    console.log("[global-headers]: Set headers");
+}
+
+const exclueRequest = {
+    label: 'Exclude global headers',
+    action: async (context, data) => {
+        const { request } = data;
+        context.store.setItem(`excluded.${request._id}`, "true");
+    },
+};
+
+const excludeRequestGroup = {
+    label: "Exclude global headers",
+    action: async (context, data) => {
+        const { requests } = data;
+        for (const request of requests) {
+            await context.store.setItem(`excluded.${request._id}`, "true");
+        }
+    },
+};
+
+const includeRequest = {
+    label: 'Include global headers',
+    action: async (context, data) => {
+        const { request } = data;
+        context.store.removeItem(`excluded.${request._id}`);
+    }
+};
+
+const includeRequestGroup = {
+    label: "Include global headers",
+    action: async (context, data) => {
+        const { requests } = data;
+        for (const request of requests) {
+            await context.store.removeItem(`excluded.${request._id}`);
+        }
+    }
+};
+
+exports.requestHooks = [setHeaders];
+exports.requestActions = [exclueRequest, includeRequest];
+exports.requestGroupActions = [excludeRequestGroup, includeRequestGroup];
